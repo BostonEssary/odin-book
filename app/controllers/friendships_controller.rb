98a3ca93cac44d25fd6ddb3da4c159520507b957
Boston <BostonCEssary@gmail.com>
2023-08-31 16:01:55 -0700

    add styling to posts index and adjust friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,10 +9,12 @@ class FriendshipsController < ApplicationController
         @friend = User.find(params[:id])
 
         friendship_params = {user: @user, friend: @friend}
+        inverse_friendship_params = {user: @friend, friend: @user}
 
         @friendship = @user.friendships.new(friendship_params)
+        @inverse_friendship = @friend.friendships.new(inverse_friendship_params)
 
-        if @friendship.save
+        if @friendship.save && @inverse_friendship.save
             redirect_to users_path
         end
     end
