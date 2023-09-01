9c981444d267b521c2994013dfc9471bc0f8e395
Boston <bostoncessary@gmail.com>
2023-08-31 18:28:29 -0700

    add basic seed data for users

--- db/migrate/20230825224048_add_friends_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFriendsToUsers < ActiveRecord::Migration[7.0]
-  def change
-    t.references :user, index: true
-    t.references :friend, index: true
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(
+    [
+        {
+            email: "boston@boston.com",
+            password: "password",
+            name: "Boston",
+            location: "Casa Grande, AZ",
+            bio: "Just a guy trying to be a dude. Love hip-hop and computers."
+        },
+        {
+            email: "cassie@cassie.com",
+            password: "password",
+            name: "Cassie",
+            location: "Casa Grande, AZ",
+            bio: "Heeeeyyyyyy! I'm Cassie aka CassietoCold aka Cas aka that queen. I love to dance, paint, stream and Post Malone!"
+        }
+    ]
+)
