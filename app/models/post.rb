81ad66bb9a62695d742992e4fc44a437da849453
Boston <bostoncessary@gmail.com>
2023-09-03 19:48:01 -0700

    add new function to post model that formats the created_at date/time to be more readable for the user

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
 
   has_many :likes
   has_many :liking_users, :through => :likes, :source => :user
+
+
+  def date_format
+    created_at.strftime("%B %e, %Y @ %l:%M %P")
+  end
 end
