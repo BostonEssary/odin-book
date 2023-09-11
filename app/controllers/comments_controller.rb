a3178d9f1ad9fc11020071d3521c74db2d1aa400
Boston <BostonCEssary@gmail.com>
2023-09-11 09:52:56 -0700

    add turbo_stream for post comments and add formating and styling to users#index view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,10 @@ class CommentsController < ApplicationController
     @comment = post.comments.new(comment_params)
     @comment.user = current_user
     @comment.save
-  
+    
+    respond_to do |format|
+      format.turbo_stream 
+    end
   end
 
   def new
