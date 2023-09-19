ba8e9993484a1169ae674f3119c799e07dad536f
Boston <BostonCEssary@gmail.com>
2023-09-19 15:20:07 -0700

    add date to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :post
   has_many :likes, as: :likeable
   has_noticed_notifications
+
+
+  def date_format
+    created_at.strftime("%B %e, %Y @ %l:%M %P")
+  end
 end
